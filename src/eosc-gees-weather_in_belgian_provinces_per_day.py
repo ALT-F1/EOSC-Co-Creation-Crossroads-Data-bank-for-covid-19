4da4114e7ccf53c3b26d35b548c1b4eebe955f46
Abdelkrim Boujraf <abo+git@alt-f1.be>
2020-06-08 09:50:12 +0200

    feat: create a directory by_date/yyy-mm-dd/*.{json|csv} containing OpenWeatherMap.org for Belgian cities, and group the weather data by province, and add quantiles 25-50-75 for main.temp, main.feels_like, main.pressure, main.humidity, main.temp, wind.speed, wind.deg

--- src/eosc-gees-weather_in_belgian_provinces_per_day.py
+++ src/eosc-gees-weather_in_belgian_provinces_per_day.py
@@ -27,6 +27,9 @@ import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
+from time import perf_counter
+import time
+import re
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
@@ -45,6 +48,17 @@ class BelgianCities():
     def __init__(self):
         self.altF1BeHelpers = AltF1BeHelpers()
 
+        # %% [code]
+        # keep: get Belgian Postal codes
+        self.bpost_postal_codes = BPost_postal_codes()
+        print(self.bpost_postal_codes.df_postal_codes_in_be)
+
+        # %% [code]
+        # keep: get Belgian cities from OpenWeatherMap
+        self.openWeatherMap = OpenWeatherMap()
+        self.openWeatherMap.build_df()
+        print(self.openWeatherMap.df_cities_weather_in_be)
+
     def thread_save(self, single_date):
         self.df_merged.apply(
             belgianCities.save_to_file, axis=1, args=[single_date.year, single_date.month, single_date.day, 'csv'])
@@ -58,10 +72,17 @@ class BelgianCities():
             # set end_date to yesterday to ensure that the weather data exist for 24 hours
             end_date = datetime.now() - timedelta(0)
 
+        list_threads = []
         # see https://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python
         for single_date in self.altF1BeHelpers.daterange(start_date, end_date):
             x = threading.Thread(target=self.thread_save, args=(single_date,))
+            list_threads.append(x)
             x.start()
+
+        print(f"All threads are started: {len(list_threads)}")
+
+        for t in list_threads:
+            t.join()  # Wait until thread terminates its task
 
     def save_to_file(self, current_row, year=2020, month=5, day=1, format='csv'):
         """
@@ -75,12 +96,12 @@ class BelgianCities():
         start_datetime, end_datetime = self.openWeatherMap.get_range_between_days(
             year, month, day)
 
-        filename = os.path.join(self.altF1BeHelpers.output_directory([start_datetime.strftime("%Y-%m-%d")]),
+        filename = os.path.join(self.altF1BeHelpers.output_directory(['by_date', start_datetime.strftime("%Y-%m-%d")]),
                                 f'{city_postal_code.zfill(4)}-{city_name}-{start_datetime.strftime("%Y-%m-%d")}')
 
         if os.path.exists(f"{filename}.json"):
             print(
-                f"File already exists: We skip its retrieval from OpenWeathMap.org: {filename}.json")
+                f"WARNING: File already exists: We skip its retrieval from OpenWeathMap.org: {filename}.json")
             with open(f"{filename}.json") as json_file:
                 weather_json = json.load(json_file)
             return weather_json
@@ -109,20 +130,10 @@ class BelgianCities():
         """
         Merge OpenWeatherMap.org and BPost.be cities list and postal codes respectivelly. 
         """
-        # %% [code]
-        # keep: get Belgian cities from OpenWeatherMap
-        self.openWeatherMap = OpenWeatherMap()
-        self.openWeatherMap.build_df()
-        print(self.openWeatherMap.df_cities_weather_in_be)
-
-        # %% [code]
-        # keep: get Belgian Postal codes
-        bpost_postal_codes = BPost_postal_codes()
-        print(bpost_postal_codes.df_postal_codes_in_be)
 
         # %% [code]
         # Transform: merge df_postal_codes_in_be, df_cities_weather_in_be, how='right'"
-        merged_right = pd.merge(bpost_postal_codes.df_postal_codes_in_be, self.openWeatherMap.df_cities_weather_in_be, how='right', left_on=[
+        merged_right = pd.merge(self.bpost_postal_codes.df_postal_codes_in_be, self.openWeatherMap.df_cities_weather_in_be, how='right', left_on=[
                                 'Localité normalized'], right_on=['city.findname'], validate='many_to_one')
 
         # Find Belgian localité that did not find a Postal code : knokke-heist, chasse royale, plombieres, cureghem, brunehault
@@ -148,8 +159,14 @@ class BelgianCities():
                 columns=['Province', 'Code postal', 'Commune principale', 'Localité', 'Commune principale normalized', 'Localité normalized'])
 
             # Transform : Merge localité and city.findname IF code postal is NaN
-            merged_right_localite = pd.merge(bpost_postal_codes.df_postal_codes_in_be, merged_right_localite, how='right', left_on=[
-                                             'Commune principale normalized'], right_on=['city.findname'], validate='many_to_one')
+            merged_right_localite = pd.merge(
+                self.bpost_postal_codes.df_postal_codes_in_be,
+                merged_right_localite,
+                how='right',
+                left_on=['Commune principale normalized'],
+                right_on=['city.findname'],
+                validate='many_to_one'
+            )
 
         self.df_merged = pd.concat(
             [merged_right_localite, merged_right]).sort_values('Code postal')
@@ -165,14 +182,217 @@ class BelgianCities():
 
         return self.df_merged
 
+    def build_df_per_province_per_quartile(self):
+        """Build a DataFrame of BPost.be data grouped by province and per quartile (25, 50 and 75) 
+
+        """
+        self.bpost_postal_codes.df_postal_codes_in_be = self.bpost_postal_codes.translate_provinces_in_french(
+            self.bpost_postal_codes.df_postal_codes_in_be)
+        return self.bpost_postal_codes.df_postal_codes_in_be.groupby(['Province'])[
+            'Code postal'].apply(set)
+
+    def append_df_per_province(self, df, filename):
+        """
+        Use the postal code of the filename to append in a DataFrame the province, the postal code and the Date in YYY-MM-DD format
+
+        Returns
+        -------
+        DataFrame
+            DataFrame appended with Three columns : Province, Postal code and date
+
+        """
+        postal_code = filename[:4]
+        province = self.bpost_postal_codes.get_province_from(postal_code=int(
+            postal_code), df=self.bpost_postal_codes_grouped_by_province)
+        df['Province'] = province
+        df['Postal code'] = postal_code
+        df['date'] = df['dt'].apply(
+            lambda x: time.strftime('%Y-%m-%d', time.localtime(x)))
+        # print(f"{postal_code}-{province}")
+        # print(df)
+        # df.columns
+        #current_province = df_postal_codes_in_be.loc[df_postal_codes_in_be['Code postal'] == df.loc['Province']]
+        return df
+
+    def add_quantiles(self, df):
+        # per province
+        columns = [
+            'message',
+            'cod',
+            'dt',
+            'Province',
+            'date',
+            'main.temp25', 'main.temp50', 'main.temp75',
+            'main.feels_like25', 'main.feels_like50', 'main.feels_like75',
+            'main.pressure25', 'main.pressure50', 'main.pressure75',
+            'main.humidity25', 'main.humidity50', 'main.humidity75',
+            'main.temp_min25', 'main.temp_min50', 'main.temp_min75',
+            'main.temp_max25', 'main.temp_max50', 'main.temp_max75',
+            'wind.speed25', 'wind.speed50', 'wind.speed75',
+            'wind.deg25', 'wind.deg50', 'wind.deg75',
+        ]
+        df_quantiles = pd.DataFrame(columns=columns)
+        #lines = 0
+
+        df_quantiles_sorted = df.sort_values(
+            ['Province', 'dt']).copy(deep=True)
+
+        for province in df_quantiles_sorted['Province'].unique():
+            # per day
+            # print(f"PROVINCE: {province}")
+            for date in df_quantiles_sorted['date'].unique():
+                # print(f"LINES: {lines} - {df_quantiles.shape}")
+                #lines = lines+1
+                # print(f"DATE : {date}")
+                # compute quantile
+                current_date_df = df_quantiles_sorted.loc[
+                    (df_quantiles_sorted['Province'] == province) & (
+                        df_quantiles_sorted['date'] == date)
+                ]
+
+                new_row = pd.DataFrame(
+                    [
+                        [
+                            current_date_df.iloc[0]['message'],
+                            current_date_df.iloc[0]['cod'],
+                            current_date_df.iloc[0]['dt'],
+                            province,
+                            date,
+                            current_date_df['main.temp'].quantile(.25),
+                            current_date_df['main.temp'].quantile(.50),
+                            current_date_df['main.temp'].quantile(.75),
+                            current_date_df['main.feels_like'].quantile(.25),
+                            current_date_df['main.feels_like'].quantile(.50),
+                            current_date_df['main.feels_like'].quantile(.75),
+                            current_date_df['main.pressure'].quantile(.25),
+                            current_date_df['main.pressure'].quantile(.50),
+                            current_date_df['main.pressure'].quantile(.75),
+                            current_date_df['main.humidity'].quantile(.25),
+                            current_date_df['main.humidity'].quantile(.50),
+                            current_date_df['main.humidity'].quantile(.75),
+                            current_date_df['main.temp_min'].quantile(.25),
+                            current_date_df['main.temp_min'].quantile(.50),
+                            current_date_df['main.temp_min'].quantile(.75),
+                            current_date_df['main.temp_max'].quantile(.25),
+                            current_date_df['main.temp_max'].quantile(.50),
+                            current_date_df['main.temp_max'].quantile(.75),
+                            current_date_df['wind.speed'].quantile(.25),
+                            current_date_df['wind.speed'].quantile(.50),
+                            current_date_df['wind.speed'].quantile(.75),
+                            current_date_df['wind.deg'].quantile(.25),
+                            current_date_df['wind.deg'].quantile(.50),
+                            current_date_df['wind.deg'].quantile(.75)
+                        ]
+                    ],
+                    columns=columns)
+
+                #new_row = pd.DataFrame([date], columns=['message'])
+                print(f"add_quantiles: new_row.shape : {new_row.shape}")
+                # print(f"new_row : {new_row}")
+                #print(f"new_row.columns : {new_row.columns}")
+                df_quantiles = df_quantiles.append(new_row, ignore_index=True)
+        return df_quantiles
+
+    def thread_save_quantiles(self, dirname, filenames):
+        index = 0
+        df_provinces_collection = pd.DataFrame()  # one dataframe per province
+        directory_by_date = os.path.basename(os.path.normpath(dirname))
+        mat = re.match('(\d{4})[/.-](\d{2})[/.-](\d{2})$',
+                       directory_by_date)
+        if mat is None:
+            print(
+                f"{directory_by_date} DOES NOT match a date, we skip the directory")
+        else:
+            filename_with_collection = os.path.join(
+                self.altF1BeHelpers.output_directory(
+                    ['by_province_and_quartile']
+                ),
+                f"{directory_by_date}.csv")
+
+            if os.path.exists(filename_with_collection):
+                print(
+                    f"WARNING: File already exists: We skip the grouping by province: {filename_with_collection}")
+            else:
+                for filename in filenames:
+                    if filename.endswith('.csv'):
+                        csv_path = os.path.join(dirname, filename)
+                        print(
+                            f"loading: {index}/{len(filenames)} - {csv_path}")
+                        df_provinces_collection = pd.concat(
+                            [
+                                df_provinces_collection,
+                                self.append_df_per_province(
+                                    pd.read_csv(csv_path, sep=','), filename
+                                )
+                            ]
+                        )
+                    index = index+1
+
+                df_quantiles = self.add_quantiles(df_provinces_collection)
+
+                # store current dataframe containing the weather of all cities from March 01 to May 22, 2020
+                df_quantiles.to_csv(
+                    f"{filename_with_collection}")
+
+                # df_quantiles.to_pickle(f"{output_filename_with_collection}.pkl")
+
+                print(
+                    f"DataFrame grouped by province and quartile are stored here : {filename_with_collection}")
+
+            df_provinces_collection = pd.DataFrame()
+
+    def create_files_grouped_by_province_with_quartiles(self):
+        """Create one file per day containing the weather data including the province and the quartiles for the temperature, the humidity and the wind
+
+        """
+        t1_start = perf_counter()
+
+        self.bpost_postal_codes_grouped_by_province = self.build_df_per_province_per_quartile()
+        openweathermap_org_weather_by_date_directory = self.altF1BeHelpers.output_directory([
+                                                                                            'by_date'])
+
+        print(
+            f"openweathermap_org_weather_by_date_directory: {openweathermap_org_weather_by_date_directory}")
+
+        list_threads = []
+
+        for dirname, _, filenames in os.walk(openweathermap_org_weather_by_date_directory):
+            x = threading.Thread(
+                target=self.thread_save_quantiles, args=(dirname, filenames))
+            list_threads.append(x)
+            x.start()
+
+        print(f"All threads are started: {len(list_threads)}")
+
+        for t in list_threads:
+            t.join()  # Wait until thread terminates its task
+
+        t1_stop = perf_counter()
+
+        print("Elapsed time:", t1_stop, t1_start)
+
+        print("Elapsed time during the whole program in seconds:",
+              t1_stop-t1_start)
+
         # %% [code]
+
+
+def test_save_json_csv_from_openweathermap(belgianCities):
+    merged_df = belgianCities.merge_openweathermap_bpost()
+    print(f"belgianCities.merge_openweathermap_bpost: {merged_df}")
+
+    # save the weather of all Belgian cities of yesterday (default behavior)
+    belgianCities.save_from_to_date()
+    # belgianCities.save_from_to_date(start_date=datetime(2020, 3, 1)) # store the weather data for a specific date
+    # belgianCities.save_from_to_date(start_date=datetime(2020, 3, 1), end_date=datetime(2020, 3, 31)) # store the weather data for and to a certain date
+
+
+def test_create_files_by_provinces_and_quartiles(belgianCities):
+    belgianCities.create_files_grouped_by_province_with_quartiles()
 
 
 if __name__ == "__main__":
     belgianCities = BelgianCities()
-    merged_df = belgianCities.merge_openweathermap_bpost()
-    print(f"belgianCities.merge_openweathermap_bpost: {merged_df}")
 
-    belgianCities.save_from_to_date() # save the weather of all Belgian cities of yesterday (default behavior)
-    # belgianCities.save_from_to_date(start_date=datetime(2020, 3, 1)) # store the weather data for a specific date
-    # belgianCities.save_from_to_date(start_date=datetime(2020, 3, 1), end_date=datetime(2020, 3, 31)) # store the weather data for and to a certain date
+    test_save_json_csv_from_openweathermap(belgianCities)
+    test_create_files_by_provinces_and_quartiles(belgianCities)
