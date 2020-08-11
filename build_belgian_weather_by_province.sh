b43ac276006e07bbde961bd309000b2c516a47d1
Abdelkrim Boujraf <abo+git@alt-f1.be>
2020-08-11 12:34:54 +0200

    feat: send an email when latest data are uploaded on GitHub

--- build_belgian_weather_by_province.sh
+++ build_belgian_weather_by_province.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-cd ~/dev/github/OpenWeatherMap/
+
 echo "start building the dataset: eosc-gees-weather_in_belgian_provinces_per_day.py"
+
+cd ~/dev/github/OpenWeatherMap/
+
 source ~/miniconda3/etc/profile.d/conda.sh
+source ~/dev/github/OpenWeatherMap/eosc.env
+source ~/dev/github/OpenWeatherMap/sendgrid.env
+
 conda activate pandas
+
 python src/eosc-gees-weather_in_belgian_provinces_per_day.py
+python src/sendgrid_helper.py
 
 echo "finished"
-
 echo "Next actions:"
 
 echo "git add output*"
